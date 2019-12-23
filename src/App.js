f5ecc94e3e1922488cbb62c6db7e38348f3297cc
kim <s158614@nishitech.ac.jp>
2019-12-23 14:38:34 +0900

    plannig movie component (Loding)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
 
 class App extends Component {
+
+    //상태값
+    state = {
+        isLoding : true,
+        movie : []
+    };
+
+    //라이프사이클 함수
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({isLoding : false})
+        }, 2000);
+    }
+
     render() {
+
+        const {isLoding} = this.state;
         return (
             <div>
-              <h1>Movie</h1>
+                {isLoding ? "Loding..." : "We are Ready!"}
             </div>
         );
     }
